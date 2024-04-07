658703d8a629408d44dcf12f9f6259df9fe6243f
whatqm <97435840+What-Question-Mark@users.noreply.github.com>
2024-04-08 09:34:09 +1000

    update compiler

--- compiler.js
+++ compiler.js
@@ -2,6 +2,7 @@
 
 const JavaScriptObfuscator = require("javascript-obfuscator");
 const fs = require("fs");
+const prettier = require("@prettier/sync");
 
 const Colours = require("./colours");
 const colours = new Colours();
@@ -27,6 +28,8 @@ try {
 
     let htmlContent = fs.readFileSync("./src/index.html", "utf8");
 
+    // Insert obfuscated javascript
+
     let scriptTagIndex = htmlContent.indexOf(
         '<script src="index.js" type="module" defer></script>'
     );
@@ -42,8 +45,13 @@ try {
             "</script>" +
             scriptContent;
     } else {
-        console.log("Script tag not found in the HTML file.");
+        console.log(
+            `${colours.FgRed}❌ Script tag not found in the HTML file${colours.Reset}`
+        );
+        return
     }
+
+    // Insert css
 
     let styleTagIndex = htmlContent.indexOf(
         '<link rel="stylesheet" href="styles.css">'
@@ -59,8 +67,55 @@ try {
             "</style>" +
             styleContent;
     } else {
-        console.log("Style tag not found in the HTML file.");
+        console.log(
+            `${colours.FgRed}❌ Style tag not found in the HTML file${colours.Reset}`
+        );
+        return
     }
+
+    // Insert developer warning
+
+    let devTagIndex = htmlContent.indexOf("// dev tag here");
+    if (devTagIndex !== -1) {
+        let devContent = htmlContent.substring(
+            devTagIndex + "// dev tag here".length
+        );
+        htmlContent =
+            htmlContent.substring(0, devTagIndex) +
+            `
+            for (let index = 0; index < 50; index++) {
+                setTimeout(() => {
+                    console.log("%cStop!", "color:red;font-family:system-ui;font-size:3rem;-webkit-text-stroke: 1px black;font-weight:bold");
+                    console.log("%cThis is a browser feature intended for developers. If someone told you to copy-paste something here it is a scam and will allow them to run code on your computer.", "color:white;font-family:system-ui;font-size:1rem;font-weight:bold");
+                }, 1000);
+            }
+            ` +
+            devContent;
+    } else {
+        console.log(
+            `${colours.FgRed}❌ Developer tag not found in the HTML file${colours.Reset}`
+        );
+        return
+    }
+
+    // Format code using prettier
+
+
+    try {
+        htmlContent = prettier.format(htmlContent, {
+            trailingComma: "es5",
+            tabWidth: 4,
+            semi: true,
+            parser: "html",
+        });
+    } catch (error) {
+        console.log(
+            `${colours.FgRed}❌ Unsuccessfully formatted code${colours.Reset}`
+        );
+        console.log(`${error}`);
+        return
+    }
+    // Write to final file
 
     fs.writeFileSync("./final.html", htmlContent);
 
