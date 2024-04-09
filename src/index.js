ed6e191eb7024e4f9a3efc878cc9968482b1b5d8
What-Question-Mark <97435840+What-Question-Mark@users.noreply.github.com>
2024-04-10 09:17:14 +1000

    pfp and discovery

--- src/index.js
+++ src/index.js
@@ -101,10 +101,17 @@ document
     .getElementById("server-list")
     .insertAdjacentHTML(
         "beforeend",
-        `<svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 448 512" class="server-sidebar-plus" id="server-sidebar-plus"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32v144H48c-17.7 0-32 14.3-32 32s14.3 32 32 32h144v144c0 17.7 14.3 32 32 32s32-14.3 32-32V288h144c17.7 0 32-14.3 32-32s-14.3-32-32-32H256z" /></svg>`
+        `<svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 448 512" id="addServer" class="server-sidebar-plus"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32v144H48c-17.7 0-32 14.3-32 32s14.3 32 32 32h144v144c0 17.7 14.3 32 32 32s32-14.3 32-32V288h144c17.7 0 32-14.3 32-32s-14.3-32-32-32H256z" /></svg>`
     );
 
-document.querySelector("#server-sidebar-plus").addEventListener("click", () => {
+document
+    .getElementById("server-list")
+    .insertAdjacentHTML(
+        "beforeend",
+        `<svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 512 512" id="findServer" class="server-sidebar-plus"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm50.7-186.9L162.4 380.6c-19.4 7.5-38.5-11.6-31-31l55.5-144.3c3.3-8.5 9.9-15.1 18.4-18.4l144.3-55.5c19.4-7.5 38.5 11.6 31 31L325.1 306.7c-3.2 8.5-9.9 15.1-18.4 18.4zM288 256a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg>`
+    ); 
+
+document.querySelector("#addServer").addEventListener("click", () => {
     let serverID = prompt("Please enter the server ID:");
     if (serverID == null || serverID == "") {
         alert("Please enter a valid server ID next time");
@@ -378,20 +385,19 @@ async function displayPosts(posts) {
     try {
         messagesDiv.innerHTML = "";
         posts.forEach((post, i) => {
-
             // time
             var momentObj = moment.unix(post.timestamp.seconds);
-            var today = moment().startOf('day');
-            var yesterday = moment().subtract(1, 'days').startOf('day');
+            var today = moment().startOf("day");
+            var yesterday = moment().subtract(1, "days").startOf("day");
             var time;
-            if (momentObj.isSame(today, 'day')) {
-                time = 'Today at ' + momentObj.format('HH:mm');
-            } else if (momentObj.isSame(yesterday, 'day')) {
-                time = 'Yesterday at ' + momentObj.format('HH:mm');
+            if (momentObj.isSame(today, "day")) {
+                time = "Today at " + momentObj.format("HH:mm");
+            } else if (momentObj.isSame(yesterday, "day")) {
+                time = "Yesterday at " + momentObj.format("HH:mm");
             } else {
-                time = momentObj.format('DD/MM/YYYY HH:mm');
+                time = momentObj.format("DD/MM/YYYY HH:mm");
             }
-            
+
             var name = post.name;
             const verified = post.verified;
             const bot = post.bot;
@@ -429,7 +435,7 @@ async function displayPosts(posts) {
 
             messageElement.className = `message${" " + mention}${" " + important}`;
             messageElement.id = post.id;
-            
+
             messageElement.innerHTML = `
             <div style="display: flex; align-items: center;">
                 <img src="https://source.boringavatars.com/beam/120/${post.name}?colors=f03333,f0b133,d0f033,33f052,33f0e0,33b1f0,a133f0" class="message-pfp">
@@ -439,19 +445,19 @@ async function displayPosts(posts) {
                     ${
                         bot === true
                             ? '<span style="color: ' +
-                                color +
-                                '; font-weight: 800;">' +
-                                name +
-                                '</span> <span style="font-weight: 400;">used</span> <span class="message-highlight" style="font-weight: 400;" onclick="document.getElementById(\'created-message\').value = \'' +
-                                command +
-                                "'.match(/\\/([^ ^!^@^#^$]+)/g) \">" +
-                                command +
-                                "</span>"
+                              color +
+                              '; font-weight: 800;">' +
+                              name +
+                              '</span> <span style="font-weight: 400;">used</span> <span class="message-highlight" style="font-weight: 400;" onclick="document.getElementById(\'created-message\').value = \'' +
+                              command +
+                              "'.match(/\\/([^ ^!^@^#^$]+)/g) \">" +
+                              command +
+                              "</span>"
                             : '<span id="message-name" style="color: ' +
-                                color +
-                                '">' +
-                                name +
-                                "</span>"
+                              color +
+                              '">' +
+                              name +
+                              "</span>"
                     }
                     <span class="message-time">${time}</span>
                 </div>
@@ -480,10 +486,13 @@ async function displayPosts(posts) {
                         previousMessageName
                     );
 
-                    const multiTime = document.createElement('span');
-                    multiTime.className = "message-time-multi"
-                    multiTime.innerHTML = `${moment.unix(post.timestamp.seconds).format("HH:mm")}`
-                    previousMessage.insertBefore(multiTime, previousMessage.children[0]);
+                    const multiTime = document.createElement("span");
+                    multiTime.className = "message-time-multi";
+                    multiTime.innerHTML = `${moment.unix(post.timestamp.seconds).format("HH:mm")}`;
+                    previousMessage.insertBefore(
+                        multiTime,
+                        previousMessage.children[0]
+                    );
                 }
             }
         });
