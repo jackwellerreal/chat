c560d92e9a7ff923024d33e8fbe5e5d8b0fa960f
What-Question-Mark <97435840+What-Question-Mark@users.noreply.github.com>
2024-04-13 22:30:32 +1000

    bug fixes 🐛🐛🐛

--- src/index.js
+++ src/index.js
@@ -194,7 +194,7 @@ async function setOnline() {
     const onlineDocSnapshot = await getDoc(onlineDocRef);
     const onlineDocData = onlineDocSnapshot.exists()
         ? onlineDocSnapshot.data()
-        : {};
+        : [];
 
     const peopleList = onlineDocData.people ? onlineDocData.people : [];
     if (peopleList.includes(name)) return;
@@ -222,7 +222,7 @@ onSnapshot(onlineDocRef, async () => {
                 </mask>
                 <foreignObject x="0" y="0" width="32" height="32" mask="url(#:r4:)">
                     <div>
-                        <img src="https://source.boringavatars.com/beam/40/${user}?colors=ED4245,FEE75C,57F287,5865F2,EB459E">
+                        <img src="https://source.boringavatars.com/beam/40/${user}?colors=ED4245,FEE75C,57F287,5865F2,EB459E" />
                     </div>
                 </foreignObject>
                 <svg x="14.5" y="17" width="25" height="15" viewBox="0 0 25 15">
@@ -247,8 +247,6 @@ onSnapshot(onlineDocRef, async () => {
 });
 
 setOnline();
-
-// Coming soon
 
 // Get the refrence to the messages
 
@@ -829,8 +827,10 @@ if (info.version == clientVersion) {
         const messageElement = document.createElement("div");
         messageElement.className = "message";
         messageElement.innerHTML = `
-        <div style="display: flex; align-items: center;">
-            <img src="https://em-content.zobj.net/source/apple/391/robot_1f916.png" class="message-pfp">
+        <div style="display: flex;">
+            <div style="height: 60px;display: flex;align-items: center;">
+                <img src="https://em-content.zobj.net/source/apple/391/robot_1f916.png" class="message-pfp">
+            </div>
             <div>
                 <div class="message-sender">
                     <span style="color: #ffff00">Bot <svg xmlns="http://www.w3.org/2000/svg" style="fill: #ffff00;" viewBox="0 0 24 24" ><path d="M22.25 12c0-1.43-.88-2.67-2.19-3.34.46-1.39.2-2.9-.81-3.91s-2.52-1.27-3.91-.81c-.66-1.31-1.91-2.19-3.34-2.19s-2.67.88-3.33 2.19c-1.4-.46-2.91-.2-3.92.81s-1.26 2.52-.8 3.91c-1.31.67-2.2 1.91-2.2 3.34s.89 2.67 2.2 3.34c-.46 1.39-.21 2.9.8 3.91s2.52 1.26 3.91.81c.67 1.31 1.91 2.19 3.34 2.19s2.68-.88 3.34-2.19c1.39.45 2.9.2 3.91-.81s1.27-2.52.81-3.91c1.31-.67 2.19-1.91 2.19-3.34zm-11.71 4.2L6.8 12.46l1.41-1.42 2.26 2.26 4.8-5.23 1.47 1.36-6.2 6.77z"/></svg>
@@ -1096,7 +1096,7 @@ form.addEventListener("submit", async (e) => {
                 if (channel.name.includes("bot")) {
                     if (message.startsWith("/verify")) {
                         if (
-                            server.codes.includes(
+                            info.codes.includes(
                                 message.replace("/verify ", "")
                             )
                         ) {
